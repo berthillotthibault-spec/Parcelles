fc49ddaf8282b3f0f660e56caae4b53cff3ef1ff
berthillotthibault-spec <berthillotthibault@gmail.com>
2026-09-14 18:54:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'parcelles-2-v4';
-const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icons/parcelles.svg','./css/base.css','./css/components.css','./css/map.css','./css/responsive.css','./js/app.js','./js/utils.js','./js/storage.js','./js/state.js','./js/import-export.js','./js/map.js','./js/sync.js'];
+const CACHE = 'parcelles-2-v6';
+const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./parcelles.svg','./base.css','./components.css','./map.css','./responsive.css','./app.js','./utils.js','./storage.js','./state.js','./import-export.js','./map.js','./sync.js'];
 const OPTIONAL_RUNTIME = ['https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css','https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.js','https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js','https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js','https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(async cache => { await cache.addAll(APP_SHELL); await Promise.allSettled(OPTIONAL_RUNTIME.map(asset => cache.add(asset))); }).then(() => self.skipWaiting())));
